9ad498ffd646dc832ecdac179fe09ce550c0c897
Cameron Winter <10819480+XtremeCookiez@users.noreply.github.com>
2021-02-25 14:41:34 -0600

    Tried another solution to ignore empty streets

--- hashcode/camerons_solution.py
+++ hashcode/camerons_solution.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 from .data_types import Street, Car
 from .input import read_file
@@ -13,7 +14,26 @@ def camerons_first_solution(time, cars, streets, intersections, output_file):
                 i.select_street(s)
                 f.write(f'{i._active_street.name} 1\n')
 
+def camerons_second_solution(time, cars, streets, intersections, output_file):
+    s_scores = defaultdict(int)
 
+    for c in cars:
+        for p in c.path:
+            s_scores[p] += 1
+
+    with open(output_file, 'w') as f:
+        f.write(f'{len(intersections)}\n')
+        for i in intersections:
+            f.write(f'{i._intersection_number}\n')
+            f.write(f'{len(i._input_streets)}\n')
+
+            o = []
+            for s in i._input_streets:
+                if s_scores[s] != 0:
+                    i.select_street(s)
+                    o.append(f'{i._active_street.name} 1\n')
+            f.write(f'{len(o)}')
+            f.write(''.join(o))
 
 
 for f in os.listdir('inputs'):
