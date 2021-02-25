42b3ef4470f9fc1529945356f1f1e1d9f3d25cf3
Cameron Winter <10819480+XtremeCookiez@users.noreply.github.com>
2021-02-25 15:01:45 -0600

    Fixed shame with more shame

--- hashcode/camerons_solution.py
+++ hashcode/camerons_solution.py
@@ -22,22 +22,28 @@ def camerons_second_solution(time, cars, streets, intersections, output_file):
             s_scores[p] += 1
 
     with open(output_file, 'w') as f:
-        f.write(f'{len(intersections)}\n')
+        # f.write(f'{len(intersections)}\n')
+        i_counter = 0
+        output_list = []
         for i in intersections:
-            f.write(f'{i._intersection_number}\n')
-            f.write(f'{len(i._input_streets)}\n')
+            # f.write(f'{i._intersection_number}\n')
 
             o = []
             for s in i._input_streets:
-                if s_scores[s] != 0:
+                if s_scores[s.name] != 0:
                     i.select_street(s)
-                    o.append(f'{i._active_street.name} 1\n')
-            f.write(f'{len(o)}')
-            f.write(''.join(o))
+                    o.append(f'{i._active_street.name} 2\n')
+            if len(o) > 0:
+                output_list.append(f'{i._intersection_number}\n')
+                output_list.append(f'{len(o)}\n')
+                output_list.append(''.join(o))
+                i_counter += 1
+        output_list.insert(0, f'{i_counter}\n')
+        f.write(''.join(output_list))
 
 
 for f in os.listdir('inputs'):
     print(f)
     input_thing = read_file(os.path.join('inputs', f))
 
-    camerons_first_solution(input_thing['time'], input_thing['cars'], input_thing['streets'], input_thing['intersections'], os.path.join('outputs', f))
+    camerons_second_solution(input_thing['time'], input_thing['cars'], input_thing['streets'], input_thing['intersections'], os.path.join('outputs', f))
