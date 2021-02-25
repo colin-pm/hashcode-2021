25beb8b24ed1467f0d61bebb087031e55959186f
Cameron Winter <10819480+XtremeCookiez@users.noreply.github.com>
2021-02-25 12:04:22 -0600

    Added some data types

---
+++ hashcode/data_types.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class Street:
+    name: str
+    length: int
+    start_intersection: int
+    end_intersection: int
+
+@dataclass
+class Car:
+    path: list
+    
