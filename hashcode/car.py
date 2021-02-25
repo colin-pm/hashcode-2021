68085282e46316ee4b75913e19ca4e52f7e15a0d
Colin McAllister <colinmca242@gmail.com>
2021-02-25 12:31:07 -0600

    Added car and street.py

---
+++ hashcode/car.py
@@ -0,0 +1,5 @@
+
+
+class Car:
+    def __init__(self, starting_street, path):
+        self.path = path
---
+++ hashcode/street.py
@@ -0,0 +1,6 @@
+
+
+class Street:
+    def __init__(self, source, dest, length):
+        self._length = length
+        self._street = []
