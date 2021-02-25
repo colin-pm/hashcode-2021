75c3a80949713002f95f75f63454064bed67a3d9
EC2 Default User <ec2-user@ip-172-31-39-125.ec2.internal>
2021-02-25 20:09:08 +0000

    moved Intersection.step() functionality into Car.step() (now complete)

--- hashcode/data_types.py
+++ hashcode/data_types.py
@@ -10,10 +10,33 @@ class Street:
     end_intersection: int
 
 
-@dataclass
 class Car:
-    path: list
-    street_remaining: int
+    def __init__(self, path):
+        self.distance = 0
+        self.path = path
+        self.street_remaining = path[0].length
+        
+    def step(self):
+        
+        # if car is done
+        if(len(path) == 0)
+            return
+
+        if(self.street_remaining == 0):
+            # car is in the intersection
+            intersection = path[0].end_intersection
+            # if we move through the intersection...
+            if(intersection.pass_car(self)):
+                # remove the path
+                self.path.pop(0)
+                # reset street_remaining for the new current street
+                self.street_remaining = path[0].length
+            
+        else:
+            # move down the street one unit
+            self.street_remaining--
+            
+
     
 
 
@@ -22,7 +45,6 @@ class Intersection:
         self._intersection_number = intersection_number
         self._input_streets = []
         self._active_street = None
-        self._changed = False
         self._queues = defaultdict(list)
 
     def add_input_street(self, street):
@@ -30,16 +52,22 @@ class Intersection:
 
     def select_street(self, street):
         self._active_street = street
-        self._changed = True
 
-    def step(self):
-        if self._changed:
-            self._changed = False
-        else:
-            if len(self._queues[self._active_street.name]):
-                next_car = self._queues[self._active_street.name].pop()
-                next_street = next_car.path.pop()
-                next_street.add_car(next_car)
+    def pass_car(self, car, street):
+
+        #skip if this street is not active
+        if(_active_street != street):
+            return false
+
+        # skip if car is not next in the queue
+        if(self._queues[street.name][0] != car)
+           return false
+
+        # remove the car from the queue
+        self._queues[street.name].pop(0)
+        
+        #indicate that the car has left the intersection
+        return true
 
     def add_car(self, car, street):
         self._queues[street.name].append(car)
